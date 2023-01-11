290c62fe714cedb6d837f2c5e00d826efa6664ee
zina06 <dhaud42@naver.com>
2023-01-11 19:47:36 +0900

    quiz 1008

---
+++ Algorithmtest/src/com/test/quiz1008.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class quiz1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		double a;
+		double b;
+		
+		a=sc.nextInt();
+		b=sc.nextInt();
+		
+		if(a>0 && b<10) {
+			
+				System.out.println(a/b);
+			
+			
+		}
+	}
+
+}
