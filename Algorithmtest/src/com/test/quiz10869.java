90c2d07cec8d99c52e51fae68eb1383f3509654a
zina06 <dhaud42@naver.com>
2023-01-13 23:12:40 +0900

    백준 1330 두 수 비교하기

--- Algorithmtest/src/com/test/quiz10869.java
+++ Algorithmtest/src/com/test/quiz10869.java
@@ -6,17 +6,24 @@ public class quiz10869 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int a=0;
-		int b=0;
+		int a;
+		int b;
 		
 		a=sc.nextInt();
 		b=sc.nextInt();
 		
-		System.out.println(a+b);
-		System.out.println(a-b);
-		System.out.println(a*b);
-		System.out.println(a/b);
-		System.out.println(a%b);
+		if(a>b) {
+			System.out.println(">");
+		}
+		else if(a<b) {
+			System.out.println("<");
+		}
+		else if(a==b) {
+			System.out.println("==");
+		}
+		
+		
+	
 	}
 
 }
