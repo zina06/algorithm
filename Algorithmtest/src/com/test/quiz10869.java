89a882a13a11476650e918704b7d3009d8ab3077
zina06 <dhaud42@naver.com>
2023-01-13 23:00:44 +0900

    백준 10869

---
+++ Algorithmtest/src/com/test/quiz10869.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class quiz10869 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=0;
+		int b=0;
+		
+		a=sc.nextInt();
+		b=sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
