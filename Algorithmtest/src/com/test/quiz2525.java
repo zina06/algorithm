a6689a12ce44d082ac00178db0e34a188c23e291
zina06 <dhaud42@naver.com>
2023-01-14 21:42:54 +0900

    오븐시계 - 반례 못찾음,,,

---
+++ Algorithmtest/src/com/test/quiz2525.java
@@ -0,0 +1,39 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class quiz2525 {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int a=0;
+		int b=0;
+		int c=0;
+		
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		
+	
+		if(b+c<60) {
+			System.out.println(a+" "+(b+c));
+		
+		}
+		
+	
+		else if(b+c>=60 && a+((b+c)/60)<23) {
+			System.out.println(a+((b+c)/60)+" "+((b+c)-(60*((b+c)/60))));
+			
+		}
+		
+		
+		else if(a+((b+c)/60)>=24) {
+			System.out.println((a+((b+c)/60)-24)+" "+((b+c)-(60*((b+c)/60))));
+		}
+	
+	}
+	
+
+}
